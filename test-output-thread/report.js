e954ed813a2caa6913884319e7e232b2d25d0f04
Erjon Domi <erjondomi@hotmail.com>
2021-03-19 00:07:38 -0400

    initial commit1

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "f4f6141b-270d-467b-871b-16c3ed6f121e",
+    "feature": "user status feature",
+    "scenario": "verify user status",
+    "start": 1616126737552,
+    "group": 1,
+    "content": "",
+    "tags": "@status,",
+    "end": 1616126759395,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
